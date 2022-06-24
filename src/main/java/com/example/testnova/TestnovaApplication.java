2dcb63987951ad7261b85b3d65e6903c15068ad6
Vo Quang Hoa <hvquang@openwt.com>
2022-06-24 10:16:22 +0700

    init

---
+++ src/main/java/com/example/testnova/TestnovaApplication.java
@@ -0,0 +1,13 @@
+package com.example.testnova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestnovaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestnovaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testnova/TestnovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testnova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestnovaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
